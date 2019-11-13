4e54fc5a79348ccf5bace17532f5015eb92052d2
fa.zubairov <fa.zubairov@tensor.ru>
2019-11-13 18:05:45 +0200

    Server & proxy (nonworking)

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+    return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+    return res.send('serve');
+    // res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, function () {
+    console.log(`App listening on port ${port}!`);
+});
