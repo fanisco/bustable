81ae7acf01105052ce8deed568bede4b89c22484
fa.zubairov <fa.zubairov@tensor.ru>
2019-11-19 15:24:34 +0200

    MX and DX and SKO

--- server/math/RandomVariable.js
+++ server/math/RandomVariable.js
@@ -1,5 +1,3 @@
-const values = [2.5, 1.8, 2.6, 1.6, 2.1, 2.3, 2.5, 1.3, 2.5, 2.7, 3, 2.5, 1.3, 2.4, 1.3, 2.7, 2.7, 2.4, 3, 3, 2.7, 1.1, 2.7, 2.3, 2.3, 3, 2.3, 1.4, 2.3, 1.8, 2.9, 1.8, 2.1, 1.3, 1.9, 2.8, 1.3, 3, 1.6, 1.1, 2.7, 2.2, 1.9, 1.6, 1.2, 1, 2.4, 2.6, 3, 1.8];
-
 function probability(list) {
     const length = list.length;
     const count = {};
@@ -16,5 +14,16 @@ function variance(x) {
     return expected(x.map(value => [Math.pow(value[0] - mx, 2), value[1]]));
 }
 
-console.log(expected(probability(values)));
-console.log(variance(probability(values)));
+function standard(x) {
+    const mx = variance(x);
+    return Math.sqrt(mx);
+}
+
+module.exports.px =
+module.exports.probability = probability;
+module.exports.mx =
+module.exports.expected = expected;
+module.exports.dx =
+module.exports.variance = variance;
+module.exports.standard =
+module.exports.deviation = standard;
