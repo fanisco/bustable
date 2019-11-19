8c7486d20bcbbc96dc87cb0ba85acac075784bcf
fa.zubairov <fa.zubairov@tensor.ru>
2019-11-19 15:14:26 +0200

    MX and DX

--- server/math/RandomVariable.js
+++ server/math/RandomVariable.js
@@ -1,29 +1,20 @@
 const values = [2.5, 1.8, 2.6, 1.6, 2.1, 2.3, 2.5, 1.3, 2.5, 2.7, 3, 2.5, 1.3, 2.4, 1.3, 2.7, 2.7, 2.4, 3, 3, 2.7, 1.1, 2.7, 2.3, 2.3, 3, 2.3, 1.4, 2.3, 1.8, 2.9, 1.8, 2.1, 1.3, 1.9, 2.8, 1.3, 3, 1.6, 1.1, 2.7, 2.2, 1.9, 1.6, 1.2, 1, 2.4, 2.6, 3, 1.8];
 
-function precise(value) {
-    return Math.floor(value * 1000) / 1000;
+function probability(list) {
+    const length = list.length;
+    const count = {};
+    list.map(value => count[value] = count[value] >= 0 ? count[value] + 1 : 1);
+    return Object.keys(count).map(key => [parseFloat(key), count[key] / length]);
 }
 
-function probability(random) {
-    const length = random.length;
-    const counters = {};
-    const prob = [];
-    random.map(value => counters[value] = counters[value] >= 0 ? counters[value] + 1 : 1);
-    console.log(Object.values(counters).map(value => value / length));
-    console.log(random.map(value => [value, 1 / length]));
-    return prob;
+function expected(x) {
+    return x.map(value => value[0] * value[1]).reduce((sum, i) => sum + i, 0);
 }
 
-function expected(random) {
-    const prob = probability(random);
-    const precMult = prob.map(value => precise(value[0] * value[1]));
-    return precMult.reduce((sum, i) => precise(sum + i), 0);
+function variance(x) {
+    const mx = expected(x);
+    return expected(x.map(value => [Math.pow(value[0] - mx, 2), value[1]]));
 }
 
-function variance() {
-
-}
-
-
-console.log(probability(values));
-console.log(expected(values));
+console.log(expected(probability(values)));
+console.log(variance(probability(values)));
